d859c20f972825348be2ab51199f644f9732fec7
mitchellshera <mitchellwachera@gmail.com>
2023-11-28 10:15:05 +0300

    blockscoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
+    const task = false;
     const task2 = true;
   
+    /* eslint-disable no-unused-vars */
     if (trueOrFalse) {
-      let task = true;
+      const task = true;
       const task2 = false;
     }
+    /* eslint-enable no-unused-vars */
   
     return [task, task2];
   }
-  
