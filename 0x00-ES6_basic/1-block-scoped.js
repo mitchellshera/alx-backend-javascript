3922da97d6884a209d74ad624281ca76ff080790
mitchellshera <mitchellwachera@gmail.com>
2023-11-28 10:54:21 +0300

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    /* eslint-disable no-unused-vars */
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-    }
-    /* eslint-enable no-unused-vars */
-  
-    return [task, task2];
+  let task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // Removed the unused variables
+    task = true;
+    // task2 is not modified in the conditional block, so no need to redeclare it
   }
+
+  return [task, task2];
+}
