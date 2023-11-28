0297ee59266e5968d16d73a65a61d2fe193ab6d9
mitchellshera <mitchellwachera@gmail.com>
2023-11-28 10:56:05 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // Removed the unused variables
+    // Remove the redeclaration of task and task2
     task = true;
-    // task2 is not modified in the conditional block, so no need to redeclare it
   }
 
   return [task, task2];
