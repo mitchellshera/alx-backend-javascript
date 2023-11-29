5768412dd021a7805e36846f4a400ddc4db8e163
mitchellshera <mitchellwachera@gmail.com>
2023-11-29 21:00:14 +0300

    8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-      throw new Error('Cannot divide by 0');
-    }
-  
+  if (denominator === 0) {
+    throw new Error('Cannot divide by 0');
+  } else {
     return numerator / denominator;
   }
+}
