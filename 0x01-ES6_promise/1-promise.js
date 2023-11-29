567639fa0492e9b3f5bd7abd0119637c190bf6d9
mitchellshera <mitchellwachera@gmail.com>
2023-11-29 18:32:03 +0300

    1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (success) {
+          resolve({
+            status: 200,
+            body: 'Success'
+          });
+        } else {
+          reject(new Error('The fake API is not working currently'));
+        }
+      }, 1000);
+    });
+  }
