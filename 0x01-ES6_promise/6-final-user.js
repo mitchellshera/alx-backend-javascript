75e547d313db89e6becb247435c407ee908c2c9e
mitchellshera <mitchellwachera@gmail.com>
2023-11-29 20:33:43 +0300

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,7 +8,7 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise.allSettled([userPromise, photoPromise]).then(
     (results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
     })),
   );
 }
