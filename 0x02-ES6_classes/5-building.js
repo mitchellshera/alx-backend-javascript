066bb77aa353fe47fa8cdb2994d4558636514bc5
mitchellshera <mitchellwachera@gmail.com>
2023-11-30 22:11:21 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,21 @@
 export default class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    // Abstract method that must be implemented by subclasses
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    this.sqft = sqft;
+
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
