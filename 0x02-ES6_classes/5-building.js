cfc1630d6ac5c5165389aefe80d9002f4930e7e6
mitchellshera <mitchellwachera@gmail.com>
2023-11-30 22:21:20 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    this._sqft = sqft;
 
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
