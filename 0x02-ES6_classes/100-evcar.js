9d466ea5d203fea43778937a19fd790197a9c579
mitchellshera <mitchellwachera@gmail.com>
2023-12-01 22:05:34 +0300

    TRIAL 100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,22 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  // Override cloneCar method to return an instance of Car
+  cloneCar() {
+    const CloneSymbol = this.constructor[Symbol.CloneSymbol];
+    return new CloneSymbol();
+  }
+}
