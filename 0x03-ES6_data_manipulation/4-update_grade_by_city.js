f3f43969e4a441c9c888cddba3a0a1d31474d1be
mitchellshera <mitchellwachera@gmail.com>
2023-12-05 15:44:17 +0300

    4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
-    .map((student) => {
+    .map((originalStudent) => {
+      const student = { ...originalStudent };
       const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
 
+      // If a grade is found, update the student's grade; otherwise, set it to 'N/A'
       student.grade = gradeObject ? gradeObject.grade : 'N/A';
       return student;
     });
