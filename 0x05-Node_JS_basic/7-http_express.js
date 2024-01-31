caeb5e9581822c7c6af34c23bdb90ac749c9f3fc
mitchellshera <mitchellwachera@gmail.com>
2024-01-31 17:58:47 +0300

    7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,52 +4,78 @@ const { readFile } = require('fs').promises;
 const app = express();
 const port = 1245;
 
-async function countStudents(fileName) {
-  try {
-    const data = await readFile(fileName, 'utf-8');
-    const lines = data.split('\n').filter(Boolean); // Filter out empty lines
+// Function to count students from a CSV file
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
 
-    const students = {};
-    const fields = Object.fromEntries(lines.map(line => {
-      const [firstname, , , field] = line.split(',');
+  return readFile(fileName, 'utf-8')
+    .then((data) => {
+      let output = '';
+      const lines = data.split('\n');
 
-      if (!students[field]) students[field] = [firstname];
-      else students[field].push(firstname);
+      // Loop through each line in the CSV file
+      for (let i = 0; i < lines.length; i += 1) {
+        if (lines[i]) {
+          length += 1;
+          const field = lines[i].split(',');
 
-      return [field, (fields[field] || 0) + 1];
-    }));
+          // Process each field
+          if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+            students[field[3]].push(field[0]);
+          } else {
+            students[field[3]] = [field[0]];
+          }
 
-    const totalStudents = lines.length - 1;
-    let output = `Number of students: ${totalStudents}\n`;
-
-    for (const [key, value] of Object.entries(fields)) {
-      if (key !== 'field') {
-        output += `Number of students in ${key}: ${value}. `;
-        output += `List: ${students[key].join(', ')}\n`;
+          if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+            fields[field[3]] += 1;
+          } else {
+            fields[field[3]] = 1;
+          }
+        }
       }
-    }
 
-    return output;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+      // Calculate total students
+      const totalStudents = length - 1;
+      output += `Number of students: ${totalStudents}\n`;
+
+      // Generate output for each field
+      for (const [key, value] of Object.entries(fields)) {
+        if (key !== 'field') {
+          output += `Number of students in ${key}: ${value}. `;
+          output += `List: ${students[key].join(', ')}\n`;
+        }
+      }
+
+      return output;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
 }
 
-app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
+// Route for the root endpoint
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (request, response) => {
+// Route for the /students endpoint
+app.get('/students', async (req, res) => {
   try {
+    // Fetch student data and send the response
     const output = await countStudents(process.argv[2].toString());
-    response.send(`This is the list of our students\n${output}`);
+    res.send(`This is the list of our students\n${output}`);
   } catch (error) {
-    response.send('This is the list of our students\nCannot load the database');
+    // Handle errors and send an appropriate response
+    res.status(404).send('This is the list of our students\nCannot load the database');
   }
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
+// Export the app variable
 module.exports = app;
