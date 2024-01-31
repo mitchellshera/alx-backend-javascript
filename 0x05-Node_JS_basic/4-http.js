981e0b1090adb9c9e03487189560270211ee3cbd
mitchellshera <mitchellwachera@gmail.com>
2024-01-31 16:16:08 +0300

    4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,5 +1,8 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 124;
+
 // Create an HTTP server
 const app = http.createServer((req, res) => {
   // Set the response header to plain text
@@ -10,7 +13,8 @@ const app = http.createServer((req, res) => {
 });
 
 // Make the server listen on port 1245
-app.listen(1245);
+app.listen(port, hostname, () => {
+});
 
 // Export the app variable
 module.exports = app;
