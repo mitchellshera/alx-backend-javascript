9fb3ba9f04f2bdf6f1c2321c6cf715eb56016ec9
mitchellshera <mitchellwachera@gmail.com>
2024-02-01 15:53:36 +0300

    0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,21 @@
+// 0-calcul.test.js
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-   it('should return the rounded sum of two numbers', () => {
-      assert.strictEqual(calculateNumber(1, 3), 4);
-      assert.strictEqual(calculateNumber(1, 3.7), 5);
-      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-   });
+  it('should round and return the sum for integer values', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should round and return the sum for one decimal value', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should round and return the sum for both decimal values', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should round and return the sum for larger decimal values', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
