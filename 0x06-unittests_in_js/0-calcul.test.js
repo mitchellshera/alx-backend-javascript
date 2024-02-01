d8754a081205ad3d738f1e696a71678b2c0ea1e8
mitchellshera <mitchellwachera@gmail.com>
2024-02-01 16:19:06 +0300

    test 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -14,6 +14,29 @@ describe('calculateNumber', () => {
   it('should round and return the sum for both decimal values', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
+  it('should round and return the sum for positive integer values', () => {
+    assert.strictEqual(calculateNumber(5, 8), 13);
+  });
+
+  it('should round and return the sum for negative integer values', () => {
+    assert.strictEqual(calculateNumber(-2, -3), -5);
+  });
+
+  it('should round and return the sum for zero and positive decimal value', () => {
+    assert.strictEqual(calculateNumber(0, 3.3), 3);
+  });
+
+  it('should round and return the sum for negative decimal values', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2.5), -4);
+  });
+
+  it('should round and return the sum for zero and negative decimal value', () => {
+    assert.strictEqual(calculateNumber(0, -2.8), -3);
+  });
+
+  it('should round and return the sum for zero and zero', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+});
 
   it('should round and return the sum for larger decimal values', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
