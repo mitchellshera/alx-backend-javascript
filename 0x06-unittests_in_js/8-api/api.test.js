ca78d0f561d66bea6c861816e1cb22dc3db77722
mitchellshera <mitchellwachera@gmail.com>
2024-02-01 18:12:26 +0300

    trial

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', () => {
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    // Add any additional tests if needed
+    done();
+  });
+});
